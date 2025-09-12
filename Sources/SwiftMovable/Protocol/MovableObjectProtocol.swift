91ad954f12223515933cc3062b2b5b8a001b454c
ailu <im.ailu@outlook.com>
2025-09-12 20:08:23 +0800

    update

--- Sources/SwiftMovable/Protocol/MovableObjectProtocol.swift
+++ Sources/SwiftMovable/Protocol/MovableObjectProtocol.swift
@@ -20,29 +20,35 @@ protocol MovableObjectProtocol: Identifiable {
     var width: CGFloat { get set }
     var height: CGFloat { get set }
 
-    func onDragChanged(translation: CGSize)
-    func onDragEnd()
-    static func == (lhs: Self, rhs: Self) -> Bool
-    func hash(into hasher: inout Hasher)
-}
+    // PERIPHERY: Unused function.method.instance 'onDragChanged(translation:)'
+    // func onDragChanged(translation: CGSize)
+    // PERIPHERY: Unused function.method.instance 'onDragEnd()'
+    // func onDragEnd()
+    // PERIPHERY: Unused function.operator.infix '==(_:_:)'
+    // static func == (lhs: Self, rhs: Self) -> Bool
+    // PERIPHERY: Unused function.method.instance 'hash(into:)'
+    // func hash(into hasher: inout Hasher)
+// }
 
-extension MovableObjectProtocol where Self: Hashable {
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id
-            && lhs.pos == rhs.pos
-            && lhs.rotationDegree == rhs.rotationDegree
-            && lhs.zIndex == rhs.zIndex
-            && lhs.width == rhs.width
-            && lhs.height == rhs.height
-    }
+// extension MovableObjectProtocol where Self: Hashable {
+    // PERIPHERY: Unused function.operator.infix '==(_:_:)'
+    // static func == (lhs: Self, rhs: Self) -> Bool {
+        // lhs.id == rhs.id
+            // && lhs.pos == rhs.pos
+            // && lhs.rotationDegree == rhs.rotationDegree
+            // && lhs.zIndex == rhs.zIndex
+            // && lhs.width == rhs.width
+            // && lhs.height == rhs.height
+    // }
 
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-        hasher.combine(pos.x)
-        hasher.combine(pos.y)
-        hasher.combine(rotationDegree)
-        hasher.combine(zIndex)
-        hasher.combine(width)
-        hasher.combine(height)
-    }
+    // PERIPHERY: Unused function.method.instance 'hash(into:)'
+    // func hash(into hasher: inout Hasher) {
+        // hasher.combine(id)
+        // hasher.combine(pos.x)
+        // hasher.combine(pos.y)
+        // hasher.combine(rotationDegree)
+        // hasher.combine(zIndex)
+        // hasher.combine(width)
+        // hasher.combine(height)
+    // }
 }
